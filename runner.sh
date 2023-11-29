850d9e565ea9d723a2eeebef6229c9800b70057e
Freddie Tang <ftang@mit.edu>
2023-11-29 01:11:31 -0500

    works?

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python3 removewhitespace.py
 python3 assembler.py
-iverilog -g2012 -o sim/sim.out sim/processor_tb.sv hdl/processor.sv hdl/xilinx_single_port_ram_read_first.v
+iverilog -g2012 -o sim/sim.out sim/processor_tb.sv hdl/processor.sv hdl/singleprocessor.sv hdl/xilinx_single_port_ram_read_first.v
 vvp sim/sim.out 
 open processor.vcd  
